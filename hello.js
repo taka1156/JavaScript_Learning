641b2710b033f407ff0cabd9eaecdb8957611930
taka-codenvy <taka05061@gamil.com>
2019-10-26 03:47:13 +0000

    hello.jsの削除

--- hello.js
+++ hello.js
@@ -9,5 +9,5 @@ var server = http.createServer(function (request, response) {
   response.end("Hello World\n");
 });
 
-// Listen on port 9000, IP defaults to 127.0.0.1
-server.listen(9000);
+// Listen on port , IP defaults to 127.0.0.1
+server.listen(8081);
